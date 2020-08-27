c0775083677fbf728e30f78be0c648977681f201
nrgnrg <the.richard.cooke@gmail.com>
2020-08-27 16:17:46 +0100

    feat(utils): :recycle: Converted type utils to predicates

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,33 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export const isNil = (value: any) => value === null;
+export const isNul = (value: any): value is null => value === null;
 
-export const isUndefined = (value: any) => value === void 0;
+export const isUndefined = (value: any): value is undefined => value === void 0;
 
-export const isDefined = (value: any) => !isUndefined(value) && !isNil(value);
+export const isDefined = (value: any) => !isUndefined(value) && !isNul(value);
 
-export const isString = (value: any) => typeof value === "string";
+export const isString = (value: any): value is string =>
+  typeof value === "string";
 
-export const isNumber = (value: any) => typeof value === "number";
+export const isNumber = (value: any): value is number =>
+  typeof value === "number";
 
-export const isEqual = (a: any, b: any) =>
-  JSON.stringify(a) === JSON.stringify(b);
-
-export const isFunction = (value: any) =>
+export const isFunction = (value: any): value is Function =>
   typeof value === "function" || value instanceof Function;
 
-export const isObject = (value: any) => {
+export const isObject = (value: any): value is object => {
   const type = typeof value;
   return (
-    !isNil(value) &&
+    !isNul(value) &&
     (type === "object" || type === "function") &&
     !Array.isArray(value)
   );
 };
 
-export const isEmptyObject = (value: object) =>
+export const isEmptyObject = (value: object): value is {} =>
   isObject(value) && Object.entries(value).length === 0;
 
-export const isArray = (value: any) => Array.isArray(value);
+export const isArray = (value: any): value is [] => Array.isArray(value);
+
+export const isEqual = (a: any, b: any) =>
+  JSON.stringify(a) === JSON.stringify(b);
 
 /* eslint-enable @typescript-eslint/no-explicit-any */
